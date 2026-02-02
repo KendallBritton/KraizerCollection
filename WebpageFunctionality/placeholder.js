e57fbe0aa6c61cc1f5b4aa4568e7469060f8dd3c
Kendall Britton <kendallb85@gmail.com>
2026-02-01 20:30:27 -0500

    Test placeholder code

---
+++ WebpageFunctionality/placeholder.js
@@ -0,0 +1,6 @@
+// Loading screen script
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        window.location.href = 'WebpageLayouts/placeholder.html';
+    }, 3000); // Redirect after 3 seconds
+});
